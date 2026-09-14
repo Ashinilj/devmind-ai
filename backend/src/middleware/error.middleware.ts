5429602d2a3ce587ed5f6ed2a141caa38c584153
Ashini Lj <ashini_lj@epam.com>
2026-09-14 13:33:49 +0530

    feat: add error handling middleware

--- backend/src/middleware/error.middleware.ts
+++ backend/src/middleware/error.middleware.ts
@@ -1,4 +1,5 @@
 import type { ErrorRequestHandler } from "express";
+import { AppError } from "../utils/app-error.js";
 
 export const errorHandler: ErrorRequestHandler = (
   err,
@@ -7,6 +8,15 @@ export const errorHandler: ErrorRequestHandler = (
   _next,
 ) => {
   console.error(err);
+
+  if (err instanceof AppError) {
+    res.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+
+    return;
+  }
 
   res.status(500).json({
     status: "error",
